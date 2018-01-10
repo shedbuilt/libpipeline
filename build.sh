be9aeebc9573aa98111616b8cee008c3dd68fb14
Auston Stewart <auston@shedbuilt.net>
2018-01-09 20:23:35 -0500

    Updating to 1.5.0 and fixing bootstrap

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-./configure --prefix=/usr
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
+if [ "$SHED_BUILDMODE" == 'bootstrap' ]; then
+    LIBPL_PKGCONFIG_PATH="/tools/lib/pkgconfig"
+else
+    LIBPL_PKGCONFIG_PATH="/usr/lib/pkgconfig"
+fi
+PKG_CONFIG_PATH="$LIBPL_PKGCONFIG_PATH" ./configure --prefix=/usr || exit 1
+make -j $SHED_NUMJOBS || exit 1
+make DESTDIR="$SHED_FAKEROOT" install || exit 1
