ef05b9c4bfcb00786a850f17435880a454e4daa4
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:25:32 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ "$SHED_BUILDMODE" == 'bootstrap' ]; then
+if [ "$SHED_BUILD_MODE" == 'bootstrap' ]; then
     LIBPL_PKGCONFIG_PATH="/tools/lib/pkgconfig"
 else
     LIBPL_PKGCONFIG_PATH="/usr/lib/pkgconfig"
 fi
 PKG_CONFIG_PATH="$LIBPL_PKGCONFIG_PATH" ./configure --prefix=/usr || exit 1
-make -j $SHED_NUMJOBS || exit 1
-make DESTDIR="$SHED_FAKEROOT" install || exit 1
+make -j $SHED_NUM_JOBS || exit 1
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
